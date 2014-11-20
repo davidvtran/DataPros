246be987762d39eee87bb496ca74fc3d5e614747
David Tran <davidtran@Davids-MacBook-Pro.local>
2014-11-20 01:50:43 -0800

    Set up processing framework

--- process.py
+++ process.py
@@ -7,14 +7,33 @@ def main():
 
 
 
-	keys = get_keys()
-	#Reads CSV file in as a dictionary
+	keys = get_keys() #set up columns of data
+
+	#Reads CSV file as a dictionary
 	with open('calbug_short.csv', 'rwb') as file_obj:
 		reader = csv.DictReader(file_obj, delimiter=',')
 		for row in reader:
 			list_of_dicts.append(row) #copy data into memory
 
+	#Do some processing of data here
+	process_country(list_of_dicts)
+
+
+
+
+
+
+	#Write processed data to disk
 	write_csv(keys,list_of_dicts)
+
+
+def process_country(list_of_dicts): #standardize notation for countries
+	for row in list_of_dicts:
+		country = row['Country']
+		if country == 'USA' or country == 'United States of America':
+			row['Country'] = 'United States'
+
+
 
 
 
