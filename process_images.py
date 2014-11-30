ff10b1260db818e530c5a50580d036610e66a935
davidvtran <davidtran@airbears2-10-142-31-226.AirBears2.1918.Berkeley.EDU>
2014-11-29 16:57:50 -0800

    Trying more pre-processing

--- process_images.py
+++ process_images.py
@@ -2,18 +2,41 @@
 # Pillow - $sudo easy_install Pillow or install from http://pillow.readthedocs.org/en/latest/installation.html
 # pytesseract - https://pypi.python.org/pypi/pytesseract
 
-from PIL import Image
+from PIL import Image, ImageEnhance, ImageFilter
 import pytesseract
 
 
+original = Image.open("123.jpg") # open colour image
+#image_file = original.convert('L') # convert image to black and white
+# enhancer = ImageEnhance.Sharpness(original)
 
-image_file = Image.open("123.jpg") # open colour image
-image_file = image_file.convert('L') # convert image to black and white
-image_file.save('result.jpg')
+# enhancer.enhance(4).show()
+
+sharpened = original.filter(ImageFilter.SHARPEN)
+
+supersharpened = sharpened.filter(ImageFilter.SHARPEN)
+
+for i in range(1):
+	supersharpened = supersharpened.filter(ImageFilter.SHARPEN)
 
 
-print pytesseract.image_to_string(Image.open('123.jpg'))
+bw = supersharpened.convert('L')
+greyscale = supersharpened.convert('1')
+
+
+
+sharpened.save('sharpened.jpg')
+supersharpened.save('supersharpened.jpg')
+bw.save("black_and_white.jpg")
+greyscale.save("greyscale.jpg")
+
+
+
+print pytesseract.image_to_string(original)
 print '-------------------------'
-print pytesseract.image_to_string(Image.open('result.jpg'))
-
+print pytesseract.image_to_string(sharpened)
+print '-------------------------'
+print pytesseract.image_to_string(supersharpened) 
+print '-------------------------'
+print pytesseract.image_to_string(supersharpened) 
 
