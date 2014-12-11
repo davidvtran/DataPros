6f4bf499b4de121c271aadce7d0d2815ec08953d
davidvtran <davidtran@airbears2-10-142-156-148.AirBears2.1918.Berkeley.EDU>
2014-12-11 14:34:39 -0800

    Calculate precision & recall

--- evaluateClassification.py
+++ evaluateClassification.py
@@ -21,6 +21,21 @@ def main():
 	collectionCount = 0
 	catalogIdCount = 0
 
+	dateFP = 0
+	speciesFP = 0
+	locationFP = 0
+	collectorFP = 0
+	collectionFP = 0
+	catalogIdFP = 0
+	nothingFP = 0
+
+	"""
+	True positive = correct prediction
+	True negative = correct "null" prediction
+	False Positive = classified "null" as something
+	False Negative = classified "something" as null
+	"""
+
 
 	nothingCount = 0
 	for i in range(1000,1371):
@@ -84,6 +99,21 @@ def main():
 						print "Predicted: " + gText
 						print "Should be: " + fText
 						print ''
+						if fText == 'date':
+							dateFP +=1
+						if fText == 'species':
+							speciesFP +=1
+						if fText == 'location' or fText == 'countries' or fText == 'uscounties':
+							locationFP +=1
+						if fText == 'collector':
+							collectorFP +=1
+						if fText == 'collection':
+							collectionFP +=1
+						if fText == 'catalog_id':
+							catalogIdFP +=1
+						if fText == 'nothing':
+							nothingFP +=1
+
 
 					if fText == 'nothing' and gText == 'nothing':
 						nothingCorrect +=1
@@ -110,7 +140,44 @@ def main():
 	print "CatalogId Accuracy: " + str(1.0 * catalogIdCorrect / catalogIdCount * 100) + '%'
 	print "Nothing Accuracy: " + str(1.0 * nothingCorrect / nothingCount * 100) + '%'
 
-	#print "NothingCount: " + str(nothingCount)
+	print 'dateCorrect: ' + str(dateCorrect)
+	print "dateCount: " + str(dateCount)
+	print ''
+
+	print 'speciesCorrect: ' + str(speciesCorrect)
+	print 'speciesCount: ' + str(speciesCount)
+	print ''
+
+	print 'locationCorrect: ' + str(locationCorrect)
+	print 'locationCount: ' + str(locationCount)
+	print ''
+
+	print 'collectorCorrect ' + str(collectorCorrect)
+	print 'collectorCount: ' + str(collectorCount)
+	print ''
+
+	print 'collectionCorrect: ' + str(collectionCorrect)
+	print 'collectionCount: ' + str(collectionCount)
+	print ''
+
+
+	print 'catalogIdCorrect: ' + str(catalogIdCorrect)
+	print "catalogIdCount: " + str(catalogIdCount)
+
+	print ''
+
+
+	print 'dateFP: ' + str(dateFP)
+	print 'speciesFP: ' + str(speciesFP)
+	print 'locationFP: ' + str(locationFP)
+	print 'collectorFP: ' + str(collectorFP)
+	print 'collectionFP: ' + str(collectionFP)	
+	print 'catalogIdFP: ' + str(catalogIdFP)
+
+	print ''
+	print 'nothingCorrect: ' + str(nothingCorrect)
+	print "NothingCount: " + str(nothingCount)
+	print 'NothingFP: ' + str(nothingFP)
 
 
 if __name__ == '__main__':
