1b151fe090bf49efb21f7b4d08b6fe8091e5f9bd
Aaron Chen <achen502@gmail.com>
2014-12-10 17:28:01 -0800

    added code for accuracy per file

--- evaluateTextOutput.py
+++ evaluateTextOutput.py
@@ -12,7 +12,7 @@ def main():
 			f = list(open(scriptOutput,'r'))
 			g = list(open(manuallyTranscribed,'r'))
 
-			
+
 			if len(f) != len(g):
 				x+=1
 				print "Mismatch of number of lines in file #" + str(i)
@@ -20,7 +20,7 @@ def main():
 			for i in range(len(f)):
 				try:
 					distance = StringMatcher(None,f[i],g[i]).distance()
-					numCorrect += len(g[i]) - distance
+					numCorrect += len(g[i])-distance
 					numTotal += len(g[i])
 				except:
 					continue
@@ -32,7 +32,41 @@ def main():
 	print "Number of correct predictions: " + str(numCorrect)
 	print "Total number of characters: " + str(numTotal)
 	print "Accuracy: " + str(1.0 * numCorrect / numTotal * 100) + '%'
+	print('**************************************************************')
 
+	#added code to get accuracy per file
+	accuracy_list = []
+	for i in range(1000,5000):
+		try:
+			scriptOutput = 'text_output/processed_' + str(i) + '.txt'
+			manuallyTranscribed = 'manually transcribed results/' + str(i) + '.txt'
+			f = list(open(scriptOutput,'r'))
+			g = list(open(manuallyTranscribed,'r'))
+
+
+			numCorrect = 0
+			numTotal = 0
+
+			for j in range(len(f)):
+				try:
+					distance = StringMatcher(None,f[j],g[j]).distance()
+					numCorrect += len(g[j]) - distance
+					numTotal += len(g[j])
+					print((numCorrect,numTotal))
+				except:
+					continue
+			if (numCorrect< 0):
+				numCorrect = 0
+			accuracy_list.append((1.0*numCorrect)/numTotal*100)
+
+		except IOError:
+			continue
+	print(accuracy_list)
+	sum = 0
+	for i in accuracy_list:
+		sum+=i
+
+	print(1.0*sum/len(accuracy_list))
 
 if __name__ == '__main__':
 	main()
